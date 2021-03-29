a3c02bbf95a311eaa54d308fa76f44734ecaad8c
Minjun Kang <me@koonta.kr>
2021-03-29 17:54:06 +0900

    feature:접속에 성공하면 로그인하라

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+require('dotenv').config();
 const WebSocket = require('ws');
 
 const ws = new WebSocket('wss://baduki.progames.world');
 
+const sendMessage = (type, value) => {
+    ws.send(JSON.stringify({
+        'type': type,
+        'value': encodeURIComponent(JSON.stringify(value))
+    }));
+};
+
+const gotMessage = (type, value) => {
+    console.log(type, value);
+};
+
 ws.on('open', () => {
     console.log('Successfully connected!');
+    sendMessage('UserLogin', {
+        'token': process.env.TOKEN
+    });
 });
 
 ws.on('message', data => {
-    console.log(data);
+    const message = JSON.parse(data);
+
+    if (message.type === 'PING') {
+        sendMessage('PONG', {});
+    } else {
+        const type = message.type, value = JSON.parse(decodeURIComponent(message.value));
+        gotMessage(type, value);
+    }
 });
